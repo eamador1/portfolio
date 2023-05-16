646bed74f6830a588243e7418f2237abcdf65a2c
erwin amador <eamador@gmail.com>
2023-05-15 20:56:06 -0600

    Create javascript file for mobile menu

---
+++ script.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector('.Hamburger-Image');
+const menu = document.querySelector('.button-list');
+const closemenu = document.querySelector('.x-close');
+
+
+  
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+
+  
+  
+  
+  
+
