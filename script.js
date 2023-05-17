b8f3058b04235182c7827a71ad1a1ba48c1cc926
erwin amador <eamador@gmail.com>
2023-05-16 20:14:51 -0600

    Add code to create responsive menu

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ const menu = document.querySelector('.button-list');
 const closemenu = document.querySelector('.x-close');
 
 
-  
-hamburger.addEventListener('click', () => {
-  /*hamburger.classList.add('active');*/
-  menu.classList.add('active');
-});
 
   
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
+
+closemenu.addEventListener('click', () => {
+  menu.classList.remove('active');
+});
+
+document.querySelectorAll('.navlink').forEach (n=> n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  menu.classList.remove('active');
+}))
+
   
   
 
