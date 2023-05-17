94f40a3d2160c588011100d123d7bff830ba2bf2
erwin amador <eamador@gmail.com>
2023-05-17 08:55:28 -0600

    Change class to image

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const hamburger = document.querySelector('.Hamburger-Image');
+const hamburger = document.querySelector('.hamburger-image');
 const menu = document.querySelector('.button-list');
 const closemenu = document.querySelector('.x-close');
 hamburger.addEventListener('click', () => {
