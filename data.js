483bd9b7d0dfa2d9feac5165411e40cccc0c6631
erwin amador <eamador@gmail.com>
2023-05-19 11:14:50 -0600

    Store cards data in JS

--- data.js
+++ data.js
@@ -1,1 +1,52 @@
-const projectInfo
+const projectInfo = [
+    {
+        image: './img/Img-Placeholder1.png',
+        title: 'Professional Art printing<br>Data More',
+        text: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry´s standard.',
+        tools: ['HTML', 'Bootstrap', 'Ruby'],
+        live: 'https://eamador1.github.io/portfolio/',
+        code: 'https://github.com/eamador1/portfolio/blob/main/index.html',
+    },
+    {
+        image: './img/image-background2.png',
+        title: 'Data Dashboard<br>Healthcare',
+        text: 'A daily selection of privately personalized reads; no accounts or sign-ups required.Has been the industry´s standard.',
+        tools: ['HTML', 'Bootstrap', 'Ruby'],
+        live: 'https://eamador1.github.io/portfolio/',
+        code: 'https://github.com/eamador1/portfolio/blob/main/index.html',
+    },
+    {
+        image: './img/image-background3.png',
+        title: 'Website Portfolio', 
+        text: 'A daily selection of privately personalized reads; no accounts or sign-ups required.Has been the industry´s standard.',
+        tools: ['HTML', 'Bootstrap', 'Ruby'],
+        live: 'https://eamador1.github.io/portfolio/',
+        code: 'https://github.com/eamador1/portfolio/blob/main/index.html',
+    },
+    {
+        image: './img/Img Placeholder1.png',
+        title: 'Professional Art printing<br>Data More',
+        text: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry´s standard.',
+        tools: ['HTML', 'Bootstrap', 'Ruby'],
+        live: 'https://eamador1.github.io/portfolio/',
+        code: 'https://github.com/eamador1/portfolio/blob/main/index.html',
+    },
+    {
+        image: './img/image-background2.png',
+        title: 'Data Dashboard<br>Healthcare',
+        text: 'A daily selection of privately personalized reads; no accounts or sign-ups required.Has been the industry´s standard.',
+        tools: ['HTML', 'Bootstrap', 'Ruby'],
+        live: 'https://eamador1.github.io/portfolio/',
+        code: 'https://github.com/eamador1/portfolio/blob/main/index.html',
+    },
+    {
+        image: './img/image-background3.png',
+        title: 'Website Portfolio', 
+        text: 'A daily selection of privately personalized reads; no accounts or sign-ups required.Has been the industry´s standard.',
+        tools: ['HTML', 'Bootstrap', 'Ruby'],
+        live: 'https://eamador1.github.io/portfolio/',
+        code: 'https://github.com/eamador1/portfolio/blob/main/index.html',
+    },
+    ];
+
+    export { projectInfo };
