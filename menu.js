dcfb52be7675d94d75d5afc3516c60b2f8e57131
erwin amador <eamador@gmail.com>
2023-05-22 09:51:38 -0600

    Create js file to show and hide mobile menu

---
+++ menu.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger-image');
+const menu = document.querySelector('.button-list');
+const closemenu = document.querySelector('.x-close');
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
+closemenu.addEventListener('click', () => {
+  menu.classList.remove('active');
+});
+document.querySelectorAll('.navlink').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  menu.classList.remove('active');
+}));
