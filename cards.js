09041fea4d568bad78256eb6a3ac5da50cdf30e7
erwin amador <eamador@gmail.com>
2023-05-23 10:31:27 -0600

    Make changes to fix card display, not done yet

--- cards.js
+++ cards.js
@@ -9,25 +9,15 @@ function showCards() {
   
     const cardHTML = `
 
-    <div class="card card${index}">
+    <div class="card">
     <div class="container-card-image">
-    <img class="card-image" src="${project.image}">
     <img class="card-image1" src="${project.image1}">
-      <div>
-      <div class="container-card-heading">
-        <h4 class="card-heading">${project.title}</h4>
-      </div>    
-      <div class="container-card-paragraph">
-        <p class="card-paragraph">${project.text}</p>
-      </div>
-      <ul class="cardlist">
-        ${project.tools.map(tool => `<li class="list bootstrap">${tool}</li>`).join('')}
-      </ul>
-      <div class="container-seeproject-button">
-        <button class="button seeproject2">See Project</button>
-      </div>
+    <img class="card-image" src="${project.image}">
+    <div>
+    <div class="container-card-heading">
+    <h4 class="card-heading">${project.title}</h4>
+    </div>    
     </div>
-
     `;
   
     cardsHTML += cardHTML;
