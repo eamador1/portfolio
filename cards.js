f853fe0442de1910ef01a06694a0b37e1caf2b99
erwin amador <eamador@gmail.com>
2023-05-23 13:24:51 -0600

    Create cards from cards.js file

---
+++ cards.js
@@ -0,0 +1,23 @@
+import { projectInfo } from './data.js';
+
+const projectCards = document.querySelector('.container-cards');
+
+function showCards() {
+  let cardsHTML = '';
+
+  projectInfo.forEach((project, index) => {
+  
+    const cardHTML = `
+
+    <div class="card">
+    <div class="container-card-image">
+    <img class="card-image" src="${project.image}">
+    <div>
+    <div class="container-card-heading">
+    <h4 class="card-heading">${project.title}</h4>
+    </div>    
+    </div>
+    `;
+  
+    cardsHTML += cardHTML;
+  });
