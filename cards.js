a25919a396553c19f03fe391396518295c0e60e0
erwin amador <eamador@gmail.com>
2023-05-23 17:08:04 -0600

    Create template

--- cards.js
+++ cards.js
@@ -1,28 +1,12 @@
 import { projectInfo } from './data.js';
+const cards = document.queryselector('container-cards');
+const template = document.getElementById('template-card').content;
+const fragment = document.createDocumentFragment();
 
-const projectCards = document.querySelector('.container-cards');
-
-function showCards() {
-  let cardsHTML = '';
-
-  projectInfo.forEach((project, index) => {
+projectInfo.forEach(el => {
+  template.queryselector('img').SetAttribute('src', el.image);
+  template.queryselector('.card-heading').textContent = el.title;
+  template.queryselector('.card-paragraph').textContent = el.text;
   
-    const cardHTML = `
-
-    <div class="card">
-    <div class="container-card-image">
-    <img class="card-image" src="${project.image}">
-    <div>
-    <div class="container-card-heading">
-    <h4 class="card-heading">${project.title}</h4>
-    </div>    
-    </div>
-    `;
-  
-    cardsHTML += cardHTML;
-  });
-
-  projectCards.innerHTML = cardsHTML;
 }
-
-showCards();
+  )
