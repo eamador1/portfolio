a26d1b76b6d45cfa3af73e95700bb6069499c6e0
erwin amador <eamador@gmail.com>
2023-05-24 11:29:36 -0600

    Create elements, variables, assign values and append in container

--- cards.js
+++ cards.js
@@ -1,12 +1,46 @@
 import { projectInfo } from './data.js';
-const cards = document.queryselector('container-cards');
-const template = document.getElementById('template-card').content;
-const fragment = document.createDocumentFragment();
 
-projectInfo.forEach(el => {
-  template.queryselector('img').SetAttribute('src', el.image);
-  template.queryselector('.card-heading').textContent = el.title;
-  template.queryselector('.card-paragraph').textContent = el.text;
-  
-}
-  )
+const $containerCards = document.querySelector('.container-cards');
+projectInfo.forEach((project, index) => {
+
+
+
+// Creating Elements //
+const $cards = document.createElement('div');
+const $image = document.createElement('img');
+const $heading = document.createElement('h4');
+const $paragraph = document.createElement('p');
+const $ul = document.createElement('ul');
+const $button = document.createElement ('button');
+const $cardClassName =  `container-${(index %3) +1}`;
+
+// Assigning Classes to the Elements //
+$cards.classList.add($cardClassName);
+$image.classList.add('card-image');
+$heading.classList.add('project-title');
+$ul.classList.add('project-button');
+
+//Asigning values
+$image.setAttribute('src', project.image);
+$heading.textContent = project.title;
+$paragraph.textContent = project.text;
+$button.textContent = 'See project';
+
+project.tools.forEach ((tool) => {
+  const $li = document.createElement('li');
+  $li.classList.add('project-tools-list');
+  $li.textContent = tool;
+  $ul.appendChild($li);
+})
+
+$cards.appendChild($image);
+$cards.appendChild($heading);
+$cards.appendChild($paragraph);
+$cards.appendChild($ul);
+$cards.appendChild($li);
+$cards.appendChild($button);
+$containerCards.appendChild($cards);
+})
+
+
+
