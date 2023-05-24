c13e15166efa2acfe58df6a4301f5c935159e573
erwin amador <eamador@gmail.com>
2023-05-23 19:43:15 -0600

    Comment cards style to fix desktop display, not done yet

--- cards.js
+++ cards.js
@@ -5,17 +5,27 @@ const projectCards = document.querySelector('.container-cards');
 function showCards() {
   let cardsHTML = '';
 
-  projectInfo.forEach((project, index) => {
+  projectInfo.forEach((project) => {
   
     const cardHTML = `
 
-    <div class="card card${index}">
+    <div class="card">
     <div class="container-card-image">
     <img class="card-image" src="${project.image}">
     <div>
     <div class="container-card-heading">
     <h4 class="card-heading">${project.title}</h4>
-    </div>    
+    </div>  
+    <div class="container-card-paragraph">
+      <p class="card-paragraph">${project.text}</p>
+    </div>
+    <ul class="cardlist">
+      ${project.tools.map(tool => `<li class="list bootstrap">${tool}</li>`).join('')}
+    </ul>
+    <div class="container-seeproject-button">
+        <button class="button seeproject2">See Project</button>
+    </div>
+  
     </div>
     `;
   
