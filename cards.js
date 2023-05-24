d969ae98e7442b6429b5739d1d720536f19d5d7f
erwin amador <eamador@gmail.com>
2023-05-24 16:19:37 -0600

    Add images to storage

--- cards.js
+++ cards.js
@@ -110,6 +110,7 @@ const projects00 = [
 
 const projects = [
   {
+    images: './img/img-placeholder1.png',
     tittle:'Profesional Art Printing Data More',
     paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     languages: ['html', 'boostrap', 'Ruby'],
@@ -117,6 +118,7 @@ const projects = [
   },
 
   {
+    images: './img/image-background2.png',
     tittle:'Profesional Art Printing Data More',
     paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     languages: ['html', 'boostrap', 'Ruby'],
@@ -124,6 +126,7 @@ const projects = [
   },
 
   {
+    images: './img/image-background3.png',
     tittle:'Profesional Art Printing Data More',
     paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     languages: ['html', 'boostrap', 'Ruby'],
@@ -131,6 +134,7 @@ const projects = [
   },
 
   {
+    images: './img/img-placeholder1.png',
     tittle:'Profesional Art Printing Data More',
     paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     languages: ['html', 'boostrap', 'Ruby'],
@@ -138,6 +142,7 @@ const projects = [
   },
 
   {
+    images: './img/image-background2.png',
     tittle:'Profesional Art Printing Data More',
     paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     languages: ['html', 'boostrap', 'Ruby'],
@@ -145,6 +150,7 @@ const projects = [
   },
 
   {
+    images: './img/image-background3.png',
     tittle:'Profesional Art Printing Data More',
     paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     languages: ['html', 'boostrap', 'Ruby'],
@@ -153,43 +159,48 @@ const projects = [
 ]
 
  
-let cardWorks = document.querySelector('.card-works');
+let $cardWorks = document.querySelector('.card-works');
 
 projects.forEach((project, index) => {
   
     // Create the elements
-    let cardContainer = document.createElement('div');
-    let projectTittle = document.createElement('h2');
-    let projectDescrption = document.createElement('p');
-    let ul = document.createElement('ul');
-    let button = document.createElement('button');
-    const cardClassName = `container-${(index % 3) +1}`;
+    let $images = document.createElement('img');
+    let $cardContainer = document.createElement('div');
+    let $projectTittle = document.createElement('h2');
+    let $projectDescrption = document.createElement('p');
+    let $ul = document.createElement('ul');
+    let $button = document.createElement('button');
+    const $cardClassName = `container-${(index % 3) +1}`;
 
     // Giving clasess
-    cardContainer.classList.add(cardClassName);
-    projectTittle.classList.add('project-tittle');
-    projectDescrption.classList.add('project-description');
-    ul.classList.add('project-tools');
-    button.classList.add('project-button');
+    $images.classList.add('card-image');
+    $cardContainer.classList.add($cardClassName);
+    $projectTittle.classList.add('project-tittle');
+    $projectDescrption.classList.add('project-description');
+    $ul.classList.add('project-tools');
+    $button.classList.add('project-button');
 
     // Assing values 
-    projectTittle.textContent = project.tittle;
-    projectDescrption.textContent = project.paragraph;
-    button.textContent = project.seeBtn;
+    $images.setAttribute("src", project.images);
+    $projectTittle.textContent = project.tittle;
+    $projectDescrption.textContent = project.paragraph;
+    $button.textContent = project.seeBtn;
 
     project.languages.forEach((language) => {
-        let li = document.createElement('li');
-        li.classList.add('project-tools-list');
-        li.textContent = language;
-        ul.appendChild(li);
+        let $li = document.createElement('li');
+        $li.classList.add('project-tools-list');
+        $li.textContent = language;
+        $ul.appendChild($li);
     })
-
+  
     /* Append elements to cardContainer */
-    cardContainer.appendChild(projectTittle);
-    cardContainer.appendChild(projectDescrption);
-    cardContainer.appendChild(ul);
-    cardContainer.appendChild(button);
 
-    cardWorks.appendChild(cardContainer);
+    $cardContainer.appendChild($projectTittle);
+    $cardContainer.appendChild($projectDescrption);
+    $cardContainer.appendChild($ul);
+    $cardContainer.appendChild($button);
+    $cardContainer.appendChild($images);
+
+    $cardWorks.appendChild($cardContainer);
 
 })
