4928834a8f1a4972e4479618eeadb75df1e2984f
erwin amador <eamador@gmail.com>
2023-05-24 23:02:52 -0600

    Store data to be displayed in modal window

--- popup.js
+++ popup.js
@@ -1,54 +1,76 @@
 const projects = [
     {
-      imageDesktop: './img/img-placeholder1.png',
+      imageDesktop: 'img/SnapshootPortfolioDesktop.png',
+      imageMobile: 'img/SnapshootPortfolioMobile.png',
+      desktopTitle: 'Keeping track of hundreds of components website 1',
       title:'Professional Art Printing',
       paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      languages: ['html', 'boostrap', 'Ruby'],
+      languages: ['html', 'bootstrap', 'Ruby on Rails'],
       seeBtn: 'See Project'
     },
   
     {
-      images: './img/image-background2.png',
-      image1: 'img/image-hover.png',
-      title:'Data Dashboard Healthcare',
+      imageDesktop: 'img/SnapshootPortfolioDesktop.png',
+      imageMobile: 'img/SnapshootPortfolioMobile.png',
+      desktopTitle: 'Keeping track of hundreds of components website 2',
+      mobileTitle: 'Multi-Post Stories',
       paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      languages: ['html', 'boostrap', 'Ruby'],
+      languages: ['html', 'bootstrap', 'Ruby on Rails'],
       seeBtn: 'See Project'
     },
   
     {
-      images: './img/image-background3.png',
-      image1: 'img/image-hover.png',
-      title:'Website Portfolio',
+      imageDesktop: 'img/SnapshootPortfolioDesktop.png',
+      imageMobile: 'img/SnapshootPortfolioMobile.png',
+      desktopTitle: 'Keeping track of hundreds of components website 3',
+      mobileTitle: 'Multi-Post Stories',
       paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      languages: ['html', 'boostrap', 'Ruby'],
+      languages: ['html', 'bootstrap', 'Ruby on Rails'],
       seeBtn: 'See Project'
     },
   
     {
-      images: './img/img-placeholder1.png',
-      image1: 'img/image-hover.png',
-      title:'Professional Art Printing',
-      paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      languages: ['html', 'boostrap', 'Ruby'],
-      seeBtn: 'See Project'
+      imageDesktop: 'img/SnapshootPortfolioDesktop.png',
+      imageMobile: 'img/SnapshootPortfolioMobile.png',
+      desktopTitle: 'Keeping track of hundreds of components website 4',
+      desktopText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,', 
+      'when an unknown printer took a galley of type and scrambled it in 1960s.<br>',
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
+      'when an unknown printer took a galley of type and scrambled it in 1960s with the releax  map lapora verita.'],
+      mobileTitle: 'Multi-Post Stories',
+      mobileText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,', 
+      'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into',
+      'electronic typesetting, remaining essent'],
+      languages: ['html', 'bootstrap', 'Ruby on Rails'],
     },
   
     {
-      images: './img/image-background2.png',
-      image1: 'img/image-hover.png',
-      title:'Data Dashboard Healthcare',
-      paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      languages: ['html', 'boostrap', 'Ruby'],
-      seeBtn: 'See Project'
+      imageDesktop: 'img/SnapshootPortfolioDesktop.png',
+      imageMobile: 'img/SnapshootPortfolioMobile.png',
+      desktopTitle: 'Keeping track of hundreds of components website 5',
+      desktopText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,', 
+      'when an unknown printer took a galley of type and scrambled it in 1960s.<br>',
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
+      'when an unknown printer took a galley of type and scrambled it in 1960s with the releax  map lapora verita.'],
+      mobileTitle: 'Multi-Post Stories',
+      mobileText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,', 
+      'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into',
+      'electronic typesetting, remaining essent'],
+      languages: ['html', 'bootstrap', 'Ruby on Rails'],
     },
   
     {
-      images: './img/image-background3.png',
-      image1: 'img/image-hover.png',
-      title:'Website Portfolio',
-      paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      languages: ['html', 'boostrap', 'Ruby'],
-      seeBtn: 'See Project'
+      imageDesktop: 'img/SnapshootPortfolioDesktop.png',
+      imageMobile: 'img/SnapshootPortfolioMobile.png',
+      desktopTitle: 'Keeping track of hundreds of components website 6',
+      desktopText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,', 
+      'when an unknown printer took a galley of type and scrambled it in 1960s.<br>',
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
+      'when an unknown printer took a galley of type and scrambled it in 1960s with the releax  map lapora verita.'],
+      mobileTitle: 'Multi-Post Stories',
+      mobileText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,', 
+      'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into',
+      'electronic typesetting, remaining essent'],
+      languages: ['html', 'bootstrap', 'Ruby on Rails'],
     }
   ]
