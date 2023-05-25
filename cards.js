1e4bbda34d3ab09fae725433a69681a663df0e15
erwin amador <eamador@gmail.com>
2023-05-24 21:32:26 -0600

    Fix typo title instead of tittle in cards.js

--- cards.js
+++ cards.js
@@ -79,7 +79,7 @@ const projects00 = [
     mobileModuleText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,', 
     'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into',
     'electronic typesetting, remaining essent'],
-    desktopModuleTitle: '',
+    desktopModuleTitle: 'Keeping track of hundreds of components website',
     desktopModuleText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,', 
     'when an unknown printer took a galley of type and scrambled it in 1960s.<br>',
     'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
@@ -111,7 +111,8 @@ const projects00 = [
 const projects = [
   {
     images: './img/img-placeholder1.png',
-    tittle:'Profesional Art Printing Data More',
+    image1: 'img/image-hover.png',
+    title:'Professional Art Printing',
     paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     languages: ['html', 'boostrap', 'Ruby'],
     seeBtn: 'See Project'
@@ -119,7 +120,8 @@ const projects = [
 
   {
     images: './img/image-background2.png',
-    tittle:'Profesional Art Printing Data More',
+    image1: 'img/image-hover.png',
+    title:'Data Dashboard Healthcare',
     paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     languages: ['html', 'boostrap', 'Ruby'],
     seeBtn: 'See Project'
@@ -127,7 +129,8 @@ const projects = [
 
   {
     images: './img/image-background3.png',
-    tittle:'Profesional Art Printing Data More',
+    image1: 'img/image-hover.png',
+    title:'Website Portfolio',
     paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     languages: ['html', 'boostrap', 'Ruby'],
     seeBtn: 'See Project'
@@ -135,7 +138,8 @@ const projects = [
 
   {
     images: './img/img-placeholder1.png',
-    tittle:'Profesional Art Printing Data More',
+    image1: 'img/image-hover.png',
+    title:'Professional Art Printing',
     paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     languages: ['html', 'boostrap', 'Ruby'],
     seeBtn: 'See Project'
@@ -143,7 +147,8 @@ const projects = [
 
   {
     images: './img/image-background2.png',
-    tittle:'Profesional Art Printing Data More',
+    image1: 'img/image-hover.png',
+    title:'Data Dashboard Healthcare',
     paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     languages: ['html', 'boostrap', 'Ruby'],
     seeBtn: 'See Project'
@@ -151,13 +156,14 @@ const projects = [
 
   {
     images: './img/image-background3.png',
-    tittle:'Profesional Art Printing Data More',
+    image1: 'img/image-hover.png',
+    title:'Website Portfolio',
     paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     languages: ['html', 'boostrap', 'Ruby'],
     seeBtn: 'See Project'
   }
 ]
-
+//CODE TO DISPLAY CARDS
  
 let $cardWorks = document.querySelector('.card-works');
 
@@ -165,8 +171,9 @@ projects.forEach((project, index) => {
   
     // Create the elements
     let $images = document.createElement('img');
+    let $hoverImage = document.createElement('img');
     let $cardContainer = document.createElement('div');
-    let $projectTittle = document.createElement('h2');
+    let $projectTitle = document.createElement('h2');
     let $projectDescrption = document.createElement('p');
     let $ul = document.createElement('ul');
     let $button = document.createElement('button');
@@ -174,15 +181,17 @@ projects.forEach((project, index) => {
 
     // Giving clasess
     $images.classList.add('card-image');
+    $hoverImage.classList.add('hover-image');
     $cardContainer.classList.add($cardClassName);
-    $projectTittle.classList.add('project-tittle');
+    $projectTitle.classList.add('project-title');
     $projectDescrption.classList.add('project-description');
     $ul.classList.add('project-tools');
     $button.classList.add('project-button');
 
     // Assing values 
     $images.setAttribute("src", project.images);
-    $projectTittle.textContent = project.tittle;
+    $hoverImage.setAttribute("src", project.image1);
+    $projectTitle.textContent = project.title;
     $projectDescrption.textContent = project.paragraph;
     $button.textContent = project.seeBtn;
 
@@ -195,12 +204,24 @@ projects.forEach((project, index) => {
   
     /* Append elements to cardContainer */
 
-    $cardContainer.appendChild($projectTittle);
+    $cardContainer.appendChild($projectTitle);
     $cardContainer.appendChild($projectDescrption);
     $cardContainer.appendChild($ul);
     $cardContainer.appendChild($button);
     $cardContainer.appendChild($images);
+    $cardContainer.appendChild($hoverImage);
+    $cardContainer.appendChild($button);
 
     $cardWorks.appendChild($cardContainer);
+
+})
+
+// DISPLAY MODAL WINDOW //
+
+let $modal = document.querySelector('.modal');
+
+projects.forEach((project, index) => {
+
+  // Create the elements for in the modal section //
 
 })
