39eaf100383b219d6975051d50c4c54f734a7cdd
erwin amador <eamador@gmail.com>
2023-05-24 21:29:14 -0600

    Create popup.js to display modal window

---
+++ popup.js
@@ -0,0 +1,54 @@
+const projects = [
+    {
+      imageDesktop: './img/img-placeholder1.png',
+      title:'Professional Art Printing',
+      paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+      languages: ['html', 'boostrap', 'Ruby'],
+      seeBtn: 'See Project'
+    },
+  
+    {
+      images: './img/image-background2.png',
+      image1: 'img/image-hover.png',
+      title:'Data Dashboard Healthcare',
+      paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+      languages: ['html', 'boostrap', 'Ruby'],
+      seeBtn: 'See Project'
+    },
+  
+    {
+      images: './img/image-background3.png',
+      image1: 'img/image-hover.png',
+      title:'Website Portfolio',
+      paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+      languages: ['html', 'boostrap', 'Ruby'],
+      seeBtn: 'See Project'
+    },
+  
+    {
+      images: './img/img-placeholder1.png',
+      image1: 'img/image-hover.png',
+      title:'Professional Art Printing',
+      paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+      languages: ['html', 'boostrap', 'Ruby'],
+      seeBtn: 'See Project'
+    },
+  
+    {
+      images: './img/image-background2.png',
+      image1: 'img/image-hover.png',
+      title:'Data Dashboard Healthcare',
+      paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+      languages: ['html', 'boostrap', 'Ruby'],
+      seeBtn: 'See Project'
+    },
+  
+    {
+      images: './img/image-background3.png',
+      image1: 'img/image-hover.png',
+      title:'Website Portfolio',
+      paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+      languages: ['html', 'boostrap', 'Ruby'],
+      seeBtn: 'See Project'
+    }
+  ]
