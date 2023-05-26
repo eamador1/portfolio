b4744981a3d95515b4a02f8cec7af81011f41f1c
erwin amador <eamador@gmail.com>
2023-05-26 15:07:40 -0600

    Display warning message form validation

--- validation.js
+++ validation.js
@@ -1,7 +1,26 @@
-function emailValidation(email){
-    var expreg = /[A-Z]/;
-    var validate = expreg.test(email);
-    if (validate !== true ) {
-        alert('email should be in lower case')
-    }
+const $form = document.querySelector('.contactform')
+const $mail = document.querySelector('.email')
+const $message = document.querySelector('.warnings')
+const $closemenu = document.querySelector('.x-warning'); 
+const $container = document.querySelector('.hide')   
+    
+$form.addEventListener('submit', e => {
+    e.preventDefault()
+    let restriction = /[A-Z]/
+    let warnings = ""
+    let enter = false
+
+
+
+if (restriction.test($mail.value)) {
+    warnings+=`Warning: Please use only lower case for email`
+    enter = true
 }
+
+if (enter){
+    $message.innerHTML = warnings;
+    $container.classList.remove('hide')
+}
+
+
+})     
