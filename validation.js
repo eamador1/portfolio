a2b31077b042c7802baa07d0d97a8afde8eed6cc
erwin amador <eamador@gmail.com>
2023-05-26 10:04:47 -0600

    link validation.js to index.html

--- validation.js
+++ validation.js
@@ -1,7 +1,7 @@
 function emailValidation(email){
     var expreg = /[A-Z]/;
     var validate = expreg.test(email);
-    if (validate == false ) {
+    if (validate !== true ) {
         alert('email should be in lower case')
     }
 }
