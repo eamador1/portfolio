0f7ad377cde3b41b7abd4fbd754a4238ca0374bc
erwin amador <eamador@gmail.com>
2023-05-26 09:28:17 -0600

    Delete stored info not to be used in modal window

--- popup.js
+++ popup.js
@@ -3,30 +3,50 @@ const projects = [
       imageDesktop: 'img/SnapshootPortfolioDesktop.png',
       imageMobile: 'img/SnapshootPortfolioMobile.png',
       desktopTitle: 'Keeping track of hundreds of components website 1',
-      title:'Professional Art Printing',
-      paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      languages: ['html', 'bootstrap', 'Ruby on Rails'],
-      seeBtn: 'See Project'
+      desktopText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,', 
+      'when an unknown printer took a galley of type and scrambled it in 1960s.<br>',
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
+      'when an unknown printer took a galley of type and scrambled it in 1960s with the releax  map lapora verita.'],
+      mobileTitle: 'Multi-Post Stories',
+      mobileText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,', 
+      'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into',
+      'electronic typesetting, remaining essent'],
+      tools: ['html', 'bootstrap', 'Ruby on Rails'],
+      closeImage: 'img/x-mobile.png', 
+      liveBtn: 'See live',
+      sourceBtn: 'See source',
     },
   
     {
       imageDesktop: 'img/SnapshootPortfolioDesktop.png',
       imageMobile: 'img/SnapshootPortfolioMobile.png',
       desktopTitle: 'Keeping track of hundreds of components website 2',
+      desktopText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,', 
+      'when an unknown printer took a galley of type and scrambled it in 1960s.<br>',
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
+      'when an unknown printer took a galley of type and scrambled it in 1960s with the releax  map lapora verita.'],
       mobileTitle: 'Multi-Post Stories',
-      paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      languages: ['html', 'bootstrap', 'Ruby on Rails'],
-      seeBtn: 'See Project'
+      mobileText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,', 
+      'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into',
+      'electronic typesetting, remaining essent'],
+      tools: ['html', 'bootstrap', 'Ruby on Rails'],
+      closeImage: 'img/x-mobile.png', 
     },
   
     {
       imageDesktop: 'img/SnapshootPortfolioDesktop.png',
       imageMobile: 'img/SnapshootPortfolioMobile.png',
       desktopTitle: 'Keeping track of hundreds of components website 3',
+      desktopText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,', 
+      'when an unknown printer took a galley of type and scrambled it in 1960s.<br>',
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
+      'when an unknown printer took a galley of type and scrambled it in 1960s with the releax  map lapora verita.'],
       mobileTitle: 'Multi-Post Stories',
-      paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      languages: ['html', 'bootstrap', 'Ruby on Rails'],
-      seeBtn: 'See Project'
+      mobileText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,', 
+      'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into',
+      'electronic typesetting, remaining essent'],
+      tools: ['html', 'bootstrap', 'Ruby on Rails'],
+      closeImage: 'img/x-mobile.png',
     },
   
     {
@@ -41,7 +61,8 @@ const projects = [
       mobileText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,', 
       'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into',
       'electronic typesetting, remaining essent'],
-      languages: ['html', 'bootstrap', 'Ruby on Rails'],
+      tools: ['html', 'bootstrap', 'Ruby on Rails'],
+      closeImage: 'img/x-mobile.png',
     },
   
     {
@@ -56,7 +77,8 @@ const projects = [
       mobileText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,', 
       'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into',
       'electronic typesetting, remaining essent'],
-      languages: ['html', 'bootstrap', 'Ruby on Rails'],
+      tools: ['html', 'bootstrap', 'Ruby on Rails'],
+      closeImage: 'img/x-mobile.png',
     },
   
     {
@@ -71,24 +93,78 @@ const projects = [
       mobileText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,', 
       'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into',
       'electronic typesetting, remaining essent'],
-      languages: ['html', 'bootstrap', 'Ruby on Rails'],
+      tools: ['html', 'bootstrap', 'Ruby on Rails'],
+      closeImage: 'img/x-mobile.png',
     }
   ]
 
-  projects.forEach((project, index) =>{
-  
-  // Creating Variables//
-
-  let $seeProject = document.querySelectorAll('.project-button')
-  let $modalTitle = document.querySelector('.modal-title');
-//variables to open and close modal window//
-  let $projectButton = document.querySelectorAll('.project-button');
   let $modal = document.querySelector('.modal');
-  let $closeModal = document.querySelector('.x-modalClose');
-  let $body = document.querySelector('body');
 
-  // Assign Values//
+  projects.forEach((project, index) => {
 
-  $modalTitle.textContent = project.desktopTitle;
+    //Create Elements
+    let $modalTitle = document.createElement('div');
+    let $closeModal = document.createElement('img');
+    let $ulModal = document.createElement('ul');
+    let $pModalMobile = document.createElement('p');
+    let $live = document.createElement('button');
+    let $source = document.createElement('button');
+  
+    //Giving Classes
+    $modalTitle.classList.add('modal-title');
+    $closeModal.classList.add('x-modalClose');
+    $ulModal.classList.add('modal-tools');
+    $pModalMobile.classList.add('p-modalMobile');
+    $live.classList.add('button live');
+    $source.classList.add('button source');
 
-})
+    //Assigning Value
+    $modalTitle.textContent = project.desktopTitle;
+    $closeModal.setAttribute("src", project.closeImage);
+    $pModalMobile.textContent = project.mobileText;
+    $live.textContent = project.liveBtn;
+    $source.textContent = project.sourceBtn;
+
+    project.tools.forEach((tool) => {
+        let $liModal = document.createElement('li');
+        $liModal.classList.add('modal-list');
+        $liModal.textContent = tool;
+        $ulModal.appendChild($liModal);
+    })
+
+    $modal.appendChild($modalTitle);
+    $modal.appendChild($closeModal);
+    $modal.appendChild($pModalMobile);
+    $modal.appendChild($live);
+    $modal.appendChild($source);
+    $modal.appendChild($ulModal);
+    })
+
+  
+   
+
+
+  /*  let seePopup = () => {
+        $modal.classList.remove('hide');
+        $overlay.classList.remove('hide');
+        $body.classList.add('hide-scrollBar');
+    };
+
+    let closePopup = () => {
+        $modal.classList.add('hide');
+        $overlay.classList.add('hide');
+        $body.classList.remove('hide-scrollBar');
+    };
+
+    /*$seeProject.addEventListener('click', seePopup);
+    $closeModal.addEventListener('click', closePopup);*/
+
+*/
+
+
+
+
+        
+
+
+  
