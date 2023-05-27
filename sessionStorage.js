e804eca8967a27eaecdb42afaa5d210289dcfafc
erwin amador <eamador@gmail.com>
2023-05-27 12:33:23 -0600

    Create code in js file to store session form fields and keep them when page is reloaded

--- sessionStorage.js
+++ sessionStorage.js
@@ -1,12 +1,42 @@
-functionstore()
-
-let $name = document.querySelector('.name');
-let $mail = document.querySelector('.mail');
-let $text = document.querySelector('.message');
+let $nameField = document.querySelector('.name');
+let $mailField = document.querySelector('.email');
+let $textField = document.querySelector('.message');
 
     let information = {
-        $name = $name,
-        $mail = $mail,
-        $text = $text,     
+        $userName: '',
+        $userMail: '',
+        $textMessage: '',     
     }
-    window.sessionStorage.setItem('information',JSON.stringify(information));  
+
+    window.onload = () => {
+        const $sessionData = localStorage.getItem('userData');
+        if ($sessionData != null) {
+          const obj = JSON.parse($sessionData);
+          $nameField.value = obj.$userName;
+          $mailField.value = obj.$userMail;
+          $textField.value = obj.$textMessage;
+          information.$userName = obj.$userName;
+          information.$userMail = obj.$userMail;
+          information.$textMessage = obj.$textMessage;
+        }
+      };
+    
+      const storeData = ($sessionData) => {
+        const str = JSON.stringify($sessionData);
+        localStorage.setItem('userData', str);
+      };
+      
+      $nameField.addEventListener('change', (event) => {
+        information.$userName = event.target.value;
+        storeData(information);
+      });
+      
+      $mailField.addEventListener('change', (event) => {
+        information.$userMail = event.target.value;
+        storeData(information);
+      });
+      
+      $textField.addEventListener('change', (event) => {
+        information.$textMessage = event.target.value;
+        storeData(information);
+      });
