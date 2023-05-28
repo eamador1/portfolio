c1cc75272749d4658d8c2027063bbb9722b1c134
RuriEnciso <rurienciso@gmail.com>
2023-05-27 23:46:50 -0500

    Fixed popup window

--- cards.js
+++ cards.js
@@ -223,6 +223,8 @@ function showModal(modalId) {
   const popup = projects.find((popup) => popup.id === modalId);
 
   // Create Elements
+  const $modalContainer = document.createElement('div');
+
   const $modal = document.createElement('div');
   const $closeBtn = document.createElement('button');
   const $modalTitle = document.createElement('div');
@@ -236,6 +238,8 @@ function showModal(modalId) {
   const $divParagraph = document.createElement('div');
 
   // Giving Classes
+  $modalContainer.classList.add('modalContainer')
+
   $modal.classList.add('modal');
   $closeBtn.classList.add('x-modalClose');
   $modalTitle.classList.add('modal-title');
@@ -276,10 +280,12 @@ function showModal(modalId) {
   $aSource.appendChild($source);
   $modal.appendChild($aSource);
 
-  document.body.appendChild($modal);
+  $modalContainer.appendChild($modal);
+
+  document.body.appendChild($modalContainer);
 
   // to open tha modal
-  const modalContainer = document.querySelector('.modal');
+  const modalContainer = document.querySelector('.modalContainer');
   modalContainer.style.display = 'block';
 
   // to close modal
@@ -290,6 +296,124 @@ function showModal(modalId) {
 }
 
 const openModal = document.querySelectorAll('.project-button');
+
+const openFirstBtn = document.querySelectorAll('.see-project1');
+
+openFirstBtn.forEach((button) => {
+  button.addEventListener('click', () => {
+    // get ID of popups objects
+    // const modalId = button.dataset.projectId;
+    // call the popup function with id
+    showModal1();
+  });
+});
+
+const popup1 = 
+{
+  id: '1',
+  images: './img/img-placeholder1.png',
+  image1: 'img/image-hover.png',
+  title: 'Professional Art Printing',
+  paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+  languages: ['html', 'bootstrap', 'Ruby'],
+  seeBtn: 'See Project',
+
+  imageDesktop: 'img/SnapshootPortfolioDesktop.png',
+  imageMobile: 'img/SnapshootPortfolioMobile.png',
+  desktopTitle: 'Keeping track of hundreds of components website 1',
+  desktopText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
+    'when an unknown printer took a galley of type and scrambled it in 1960s.<br>',
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
+    'when an unknown printer took a galley of type and scrambled it in 1960s with the releax  map lapora verita.'],
+  mobileTitle: 'Multi-Post Stories',
+  mobileText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
+    'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into',
+    'electronic typesetting, remaining essent'],
+  tools: ['html', 'bootstrap', 'Ruby on Rails'],
+  closeImage: 'img/x-mobile.png',
+  liveBtn: 'See live',
+  sourceBtn: 'See source',
+  live: 'https://eamador1.github.io/portfolio/',
+  source: 'https://github.com/eamador1/portfolio/blob/main/index.html',
+}
+
+function showModal1() {
+  const popup = popup1
+
+
+  // Create Elements
+  const $modalContainer = document.createElement('div');
+
+  const $modal = document.createElement('div');
+  const $closeBtn = document.createElement('button');
+  const $modalTitle = document.createElement('div');
+  // let $closeModal = document.createElement('img');
+  const $ulModal = document.createElement('ul');
+  const $pModalMobile = document.createElement('p');
+  const $live = document.createElement('button');
+  const $aLive = document.createElement('a');
+  const $source = document.createElement('button');
+  const $aSource = document.createElement('a');
+  const $divParagraph = document.createElement('div');
+
+  // Giving Classes
+  $modalContainer.classList.add('modalContainer')
+
+  $modal.classList.add('modal');
+  $closeBtn.classList.add('x-modalClose');
+  $modalTitle.classList.add('modal-title');
+  // $closeModal.classList.add('x-modalClose');
+  $ulModal.classList.add('modal-tools');
+  $pModalMobile.classList.add('p-modalMobile');
+  $live.classList.add('live', 'button');
+  $source.classList.add('source', 'button');
+  $divParagraph.classList.add('divMP');
+
+  // Assigning Value
+  $modalTitle.textContent = popup.mobileTitle;
+  $closeBtn.innerHTML = '&times';
+  $pModalMobile.textContent = popup.mobileText;
+  $live.textContent = popup.liveBtn;
+  $source.textContent = popup.sourceBtn;
+  $aLive.href = popup.live;
+  $aLive.target = '_blank';
+  $aLive.rel = 'noopener';
+  $aSource.href = popup.source;
+  $aSource.target = '_blank';
+  $aSource.rel = 'noopener';
+
+  popup.tools.forEach((tool) => {
+    const $liModal = document.createElement('li');
+    $liModal.classList.add('modal-list');
+    $liModal.textContent = tool;
+    $ulModal.appendChild($liModal);
+  });
+
+  $modal.appendChild($modalTitle);
+  $modal.appendChild($closeBtn);
+  $modal.appendChild($ulModal);
+  $divParagraph.appendChild($pModalMobile);
+  $modal.appendChild($divParagraph);
+  $aLive.appendChild($live);
+  $modal.appendChild($aLive);
+  $aSource.appendChild($source);
+  $modal.appendChild($aSource);
+
+  $modalContainer.appendChild($modal);
+
+  document.body.appendChild($modalContainer);
+
+  // to open tha modal
+  const modalContainer = document.querySelector('.modalContainer');
+  modalContainer.style.display = 'block';
+
+  // to close modal
+  const closeModal = document.querySelector('.x-modalClose');
+  closeModal.addEventListener('click', () => {
+    modalContainer.style.display = 'none';
+  });
+}
+
 
 openModal.forEach((button) => {
   button.addEventListener('click', () => {
